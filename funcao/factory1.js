32d2d8f62c2cb3034397d5b2f763e29cd21df4e3
Leandro Alves <leandrofelipecunha@hotmail.com>
2021-04-13 18:47:04 -0300

    funcao

---
+++ funcao/factory1.js
@@ -0,0 +1,9 @@
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Kamila',
+        sobrenome: 'Siqueira'
+    }
+}
+
+console.log(criarPessoa())
---
+++ funcao/factory2.js
@@ -0,0 +1,10 @@
+// Factory com parametros
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Calculadora', 55))
