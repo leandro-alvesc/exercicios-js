74f62980278bc5334f60928bf5389113c70d8590
Leandro Alves <leandrofelipecunha@hotmail.com>
2021-04-19 18:13:33 -0300

    objetos js

---
+++ objeto/criando_objetos.js
@@ -0,0 +1,45 @@
+// usando a notação literal
+const obj1 = {}
+console.log(obj1)
+
+// Object em JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+// Funções construtoras
+function Produto(nome, preco, desc) {
+    this.nome = nome
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    }
+}
+
+const p1 = new Produto('Caneta', 1.55, 0.15)
+const p2 = new Produto('Notebook', 3556.58, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+// Função Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario('Carlos', 7890, 4)
+const f2 = criarFuncionario('Marcela', 11500, 1)
+console.log(f1.getSalario(), f2.getSalario())
+
+// Object.create
+const filha = Object.create(null)
+filha.nome = 'Roberta'
+console.log(filha)
+
+// Uma função famosa que retorna Objeto
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)
---
+++ objeto/objeto_revisao.js
@@ -0,0 +1,42 @@
+// coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Mesa'
+produto['marca do produto'] = 'Genérica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'José',
+        idade: 45,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Maria',
+        idade: 32
+    }, {
+        nome: 'Gustavo',
+        idade: 25
+    }],
+    calcularValorSeguro: function() {
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+console.log(carro)
+
+delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro)
